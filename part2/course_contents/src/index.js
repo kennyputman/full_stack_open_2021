b1d947795ba145dda6ea1d8eb7ffcbf9412ac836
Ken Putman <kbputman@gmail.com>
2020-03-30 14:57:16 -0500

    finish 2.3

--- part2/course_contents/src/index.js
+++ part2/course_contents/src/index.js
@@ -7,7 +7,7 @@ const Header = ({ course }) => {
 
 const Total = ({ course }) => {
   const sum = course.parts.reduce((sum, part) => (sum += part.exercises), 0);
-  return <h4>total of {sum} exercises </h4>;
+  return <h4>total of {sum} exercises</h4>;
 };
 
 const Part = props => {
