1c91f2f047f454ee4d2427e12a13d9204db54d52
Ken Putman <kbputman@gmail.com>
2020-03-30 14:36:54 -0500

    delete files finish 2.1

--- part2/course_contents/src/index.js
+++ part2/course_contents/src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((sum, part) => (sum += part.exercises), 0);
+  return <p>Number of exercises {sum}</p>;
+};
+
+const Part = props => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return course.parts.map(part => <Part key={part.id} part={part} />);
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: "Testy",
+        exercises: 21,
+        id: 4
+      },
+      {
+        name: "Testy2",
+        exercises: 1,
+        id: 5
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
