870d0a59c35bcccc71afe532db331fede1040a3a
Ken Putman <kbputman@gmail.com>
2020-03-31 15:27:59 -0500

    finish 2.9: add name filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newNameFilter, setNewNameFilter] = useState("");
 
   const addContact = event => {
     event.preventDefault();
@@ -27,17 +31,38 @@ const App = () => {
   const handleNumberChange = event => {
     setNewNumber(event.target.value);
   };
+  const handleNameFilterChange = event => {
+    setNewNameFilter(event.target.value);
+  };
 
-  const Numbers = ({ persons }) =>
-    persons.map(person => (
-      <li key={person.name}>
-        {person.name}: {person.number}
-      </li>
-    ));
+  const Numbers = ({ persons }) => {
+    if (newNameFilter === "") {
+      return persons.map(person => (
+        <li key={person.name}>
+          {person.name}: {person.number}
+        </li>
+      ));
+    } else {
+      return persons
+        .filter(person =>
+          person.name.toLowerCase().includes(newNameFilter.toLowerCase())
+        )
+        .map(person => (
+          <li key={person.name}>
+            {person.name}: {person.number}
+          </li>
+        ));
+    }
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with{" "}
+        <input value={newNameFilter} onChange={handleNameFilterChange} />
+      </div>
+      <h3>Add a New Contact</h3>
       <form onSubmit={addContact}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -49,7 +74,7 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <Numbers persons={persons} />
     </div>
   );
