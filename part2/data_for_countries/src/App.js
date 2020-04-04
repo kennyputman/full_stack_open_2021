313ee2e20bbe5d11945b203164497b0d40cea192
Ken Putman <kbputman@gmail.com>
2020-04-04 14:24:40 -0500

    setup countries api using axios

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+  console.log("render", countries.length, "countries");
+
+  const Country = ({ country }) => {
+    return <li>{country.name}</li>;
+  };
+
+  const CountriesList = ({ countries }) => {
+    return (
+      <ul>
+        {countries.map((country) => (
+          <Country key={country.alpha3Code} country={country} />
+        ))}
+      </ul>
+    );
+  };
   return (
     <div>
-      <p>Hello World!</p>
+      <h1>Countries</h1>
+      <CountriesList countries={countries} />
     </div>
   );
-}
+};
 
 export default App;
