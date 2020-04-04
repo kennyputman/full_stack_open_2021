7520089724a0e49bc4f5cf24111e8ce0e9ddb317
Ken Putman <kbputman@gmail.com>
2020-04-04 12:49:32 -0500

    2.11 migrate to db.json using axios and effect hook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,40 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Numbers from "./components/Numbers";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newNameFilter, setNewNameFilter] = useState("");
 
-  const addContact = event => {
+  const addContact = (event) => {
     event.preventDefault();
     const nameObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
     };
 
-    if (persons.some(person => person.name === nameObject.name)) {
+    if (persons.some((person) => person.name === nameObject.name)) {
       window.alert(`${nameObject.name} is already added to phonebook`);
     } else {
       setPersons(persons.concat(nameObject));
     }
   };
 
-  const handleNameChange = event => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
-  const handleNumberChange = event => {
+  const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
-  const handleNameFilterChange = event => {
+  const handleNameFilterChange = (event) => {
     setNewNameFilter(event.target.value);
   };
 
