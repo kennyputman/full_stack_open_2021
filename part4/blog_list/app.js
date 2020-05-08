e941accc93baca33250df3357fbdac33be00c7f1
Ken Putman <kbputman@gmail.com>
2020-05-08 17:54:19 -0500

    add PUT, DELETE, and GET by id

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
 
 logger.info("connecting to", config.MONGODB_URI);
 
@@ -23,6 +24,8 @@ mongoose
 app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
+app.use(middleware.requestLogger);
 app.use("/api/blogs", blogsRouter);
-
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 module.exports = app;
