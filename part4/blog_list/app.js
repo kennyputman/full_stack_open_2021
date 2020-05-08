1dd23a629ea90d90d7965e2fdf6f0f3ed5cf69f6
Ken Putman <kbputman@gmail.com>
2020-05-08 17:13:53 -0500

    refactor into seperate modules

---
+++ part4/blog_list/app.js
@@ -0,0 +1,28 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to mongoDB", error.message);
+  });
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
