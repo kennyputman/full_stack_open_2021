f30a92745cbec954389c64b6d4223ff1dd613550
Ken Putman <kbputman@gmail.com>
2020-12-04 16:44:18 -0600

    add blogsInDb test_helper function, finish 4.10 - 'post request creates a new blog post'

--- part4/blog_list/test/blogs_api.test.js
+++ part4/blog_list/test/blogs_api.test.js
@@ -5,10 +5,10 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
+const { initialBlogs } = require("./test_helper.js");
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  console.log("Test DB Cleared");
 
   for (let blog of helper.initialBlogs) {
     let blogObject = new Blog(blog);
@@ -31,6 +31,29 @@ test("correct number of blogs are returned", async () => {
 test("unique identifier is 'id'", async () => {
   const response = await api.get("/api/blogs");
   expect(response.body[0]).toBeDefined();
+});
+
+test("POST request creates a new blog post'", async () => {
+  const newBlog = {
+    title: "Taking the con out of econometrics",
+    author: "Edward Leamer",
+    url: "https://reactpatterns.https://www.jstor.org/stable/1803924?seq=1/",
+    likes: 1,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAfterPost = await helper.blogsInDb();
+  //check to see if a blog is added
+  expect(blogsAfterPost.length).toBe(helper.initialBlogs.length + 1);
+
+  //checks to see if blog is added and correct
+  const contents = blogsAfterPost.map((n) => n.author);
+  expect(contents).toContain("Edward Leamer");
 });
 
 afterAll(() => {
