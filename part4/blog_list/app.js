f905626b8ded80b7d6f409d3707eb3638f7e377f
Ken Putman <kbputman@gmail.com>
2020-12-08 16:35:00 -0600

    finish 4.20 - add tokenExtractor to middleware

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -3,12 +3,12 @@ const express = require("express");
 require("express-async-errors");
 const app = express();
 const cors = require("cors");
+const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
-const middleware = require("./utils/middleware");
 
 logger.info("connecting to", config.MONGODB_URI);
 
@@ -27,10 +27,15 @@ mongoose
 app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
+
 app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
+
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
+
 module.exports = app;
