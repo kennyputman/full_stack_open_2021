4ac20dffe20bcd874b6ecf0922acd647ecf0af66
Ken Putman <kbputman@gmail.com>
2020-12-21 19:08:02 -0600

    add extra info view button

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div class="blog">
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+  return (
+    <div class="blog">
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility} class="btn" style={hideWhenVisible}>
+        view
+      </button>
+      <button onClick={toggleVisibility} class="btn" style={showWhenVisible}>
+        hide
+      </button>
+      <div style={showWhenVisible} class="extraInfo">
+        {blog.url}
+        <br></br>
+        Likes: {blog.likes}
+        <button class="btn">like</button>
+        <br></br>
+        {blog.user.username}
+        <br></br>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
