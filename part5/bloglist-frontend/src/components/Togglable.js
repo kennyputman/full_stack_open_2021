1dbd59eb0bab5deeb4b55ea2fdd811c093d57dee
Ken Putman <kbputman@gmail.com>
2020-12-23 15:41:12 -0600

    add eslint and PropTypes

--- part5/bloglist-frontend/src/components/Togglable.js
+++ part5/bloglist-frontend/src/components/Togglable.js
@@ -1,4 +1,5 @@
 import React, { useState, useImperativeHandle } from "react";
+import PropTypes from "prop-types";
 
 const Togglable = React.forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false);
@@ -19,18 +20,23 @@ const Togglable = React.forwardRef((props, ref) => {
   return (
     <div>
       <div style={hideWhenVisible}>
-        <button onClick={toggleVisibility} class="btn">
+        <button onClick={toggleVisibility} classNameName="btn">
           {props.buttonLabel}
         </button>
       </div>
       <div style={showWhenVisible}>
         {props.children}
-        <button onClick={toggleVisibility} class="btn">
+        <button onClick={toggleVisibility} className="btn">
           cancel
         </button>
       </div>
     </div>
   );
 });
+
+Togglable.displayName = "Togglable";
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
 
 export default Togglable;
