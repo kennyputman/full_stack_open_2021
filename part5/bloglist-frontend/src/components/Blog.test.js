5c84ddf01664eb92b75a2dbf8cfe88c7284887f7
Ken Putman <kbputman@gmail.com>
2020-12-25 11:31:05 -0600

    finish 5.14 - add test for 'blog's url and number of likes are shown when the button controlling the shown details has been clicked.'

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,23 +1,22 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { prettyDOM } from "@testing-library/dom";
 import Blog from "./Blog";
 
+let component;
+beforeEach(() => {
+  const blog = {
+    user: { username: "Hellas" },
+    likes: 532,
+    author: "Lydia Hallie",
+    title: "JS Engine Visualzied",
+    url: "dev.to/lydiahallie/jsenginevisualized",
+  };
+  component = render(<Blog blog={blog} />);
+});
+
 describe("Blog renders Title and Author only", () => {
-  let component;
-  beforeEach(() => {
-    const blog = {
-      user: { username: "Hellas" },
-      likes: 532,
-      author: "Lydia Hallie",
-      title: "JS Engine Visualzied",
-      url: "dev.to/lydiahallie/jsenginevisualized",
-    };
-
-    component = render(<Blog blog={blog} />);
-  });
-
   test("Blog renders Title and Author", () => {
     const blogHeader = component.container.querySelector(".blogHeader");
 
@@ -31,7 +30,26 @@ describe("Blog renders Title and Author only", () => {
   test("extra info is hidden by default", () => {
     const extraInfo = component.container.querySelector(".extraInfo");
 
-    console.log(prettyDOM(extraInfo));
+    expect(extraInfo).toHaveStyle("display: none");
+  });
+});
+
+describe("Extra Info functionality Works", () => {
+  test("Clicking view button displays URL & Likes", () => {
+    const button = component.getByText("view");
+    fireEvent.click(button);
+
+    const extraInfo = component.container.querySelector(".extraInfo");
+    // console.log(prettyDOM(extraInfo));
+    expect(extraInfo).toHaveStyle("");
+  });
+
+  test("Clicking hide button hides extra info", () => {
+    const extraInfo = component.container.querySelector(".extraInfo");
+    const view = component.getByText("view");
+    const hide = component.getByText("hide");
+    fireEvent.click(view);
+    fireEvent.click(hide);
 
     expect(extraInfo).toHaveStyle("display: none");
   });
