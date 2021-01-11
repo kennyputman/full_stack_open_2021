316f3ff257d329324ffadde4bb759ff73522d4d1
Ken Putman <kbputman@gmail.com>
2021-01-11 17:13:30 -0600

    update blogs backend for put and delete requests

--- part5/bloglist-backend/controllers/blogs.js
+++ part5/bloglist-backend/controllers/blogs.js
@@ -85,7 +85,7 @@ blogsRouter.put("/:id", async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: body.user,
+    user: user._id,
   };
 
   console.log(blog);
