94d1fe19e5b7431f81f8996a6635c51147da2f83
Ken Putman <kbputman@gmail.com>
2021-01-14 17:27:01 -0600

    finish 5.18 - add test coverage for login form

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,16 +1,37 @@
 describe("Blog App", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3001/api/testing/reset");
-    // const user = {
-    //   name: "Ron apptest",
-    //   username: "ronapptest",
-    //   password: "mettaworldpeace",
-    // };
-    // cy.request("POST", "http://localhost:3001/api/users/", user);
+    const user = {
+      name: "Ron Apptest",
+      username: "ronapptest",
+      password: "mettaworldpeace",
+    };
+    cy.request("POST", "http://localhost:3001/api/users/", user);
     cy.visit("http://localhost:3000");
   });
 
   it("Login form is shown", function () {
     cy.contains("login").click();
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.contains("login").click();
+      cy.get("#username").type("ronapptest");
+      cy.get("#password").type("mettaworldpeace");
+      cy.get("#login-button").click();
+
+      cy.contains("Ron Apptest logged in");
+    });
+    it("fails with correct credentials", function () {
+      cy.contains("login").click();
+      cy.get("#username").type("shouldfail");
+      cy.get("#password").type("notreal");
+      cy.get("#login-button").click();
+
+      cy.get(".error")
+        .should("contain", "Wrong username or password")
+        .and("have.css", "color", "rgb(255, 0, 0)");
+    });
   });
 });
