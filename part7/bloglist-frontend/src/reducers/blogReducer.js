86e2c5be4557409b51b7c0d23d7c337148b97598
Kenny Putman <kbputman@gmail.com>
2021-04-14 16:15:51 -0500

    create store for blogs

--- part7/bloglist-frontend/src/reducers/blogReducer.js
+++ part7/bloglist-frontend/src/reducers/blogReducer.js
@@ -9,6 +9,8 @@ export const initBlogs = () => {
 
 const blogReducer = (state = [], action) => {
   switch (action.type) {
+    case "NEW_BLOG":
+      return [...state, action.data];
     case "INIT_BLOGS":
       return action.data;
     default:
--- part7/bloglist-frontend/src/store.js
+++ part7/bloglist-frontend/src/store.js
@@ -1,7 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
 import blogReducer from "./reducers/blogReducer";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(blogReducer, applyMiddleware(thunk));
+const store = createStore(
+  blogReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
