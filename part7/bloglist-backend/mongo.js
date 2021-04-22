70346ad3b4b6aed06790f8cca51dfbde3840e9f2
Kenny Putman <kbputman@gmail.com>
2021-04-22 17:00:04 -0500

    cleanup

--- part7/bloglist-backend/mongo.js
+++ part7/bloglist-backend/mongo.js
@@ -5,13 +5,3 @@ mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number,
-//   date: Date,
-// });
-
-// const Blog = mongoose.model("Blog", blogSchema);
