95420961899a00ac8fec0ea9c2ed1af1ff28caab
Kenny Putman <kbputman@gmail.com>
2021-06-29 14:41:20 -0500

    create reducer for patient

--- part9/patientor-frontend/src/components/PatientInfoPage.tsx
+++ part9/patientor-frontend/src/components/PatientInfoPage.tsx
@@ -1,9 +1,34 @@
+import axios from "axios";
 import React from "react";
+import { useParams } from "react-router-dom";
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+import { Patient } from "../types";
 
 const PatientInfoPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [{ patient }, dispatch] = useStateValue();
+  React.useEffect(() => {
+    void axios.get<void>(`${apiBaseUrl}/patients/${id}`);
+
+    const fetchPatient = async () => {
+      try {
+        const { data: patient } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: "SET_PATIENT", payload: patient });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    void fetchPatient();
+  }, [dispatch]);
+
+  console.log(Object.values(patient));
+
   return (
     <div className="App">
-      <p>Clicked to patient page</p>
+      <p>{Object.values(patient)[0].name}</p>
     </div>
   );
 };
